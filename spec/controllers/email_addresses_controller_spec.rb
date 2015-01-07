9a673c826d7b5468c4398f03975bf0d55d82f26b
Orion Osborn <orion.osborn@gmail.com>
2015-01-07 11:03:44 -0700

    add person_id validation for EmailAddress

--- spec/controllers/email_addresses_controller_spec.rb
+++ spec/controllers/email_addresses_controller_spec.rb
@@ -23,13 +23,9 @@ RSpec.describe EmailAddressesController, :type => :controller do
   # This should return the minimal set of attributes required to create a valid
   # EmailAddress. As you add validations to EmailAddress, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) { {address: 'test@example.com', person_id: 1} }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) { {address: nil, person_id: nil} }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
@@ -102,15 +98,14 @@ RSpec.describe EmailAddressesController, :type => :controller do
 
   describe "PUT update" do
     describe "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:new_attributes) { {address: 'updated_email@test.com', person_id: 2} }
 
       it "updates the requested email_address" do
         email_address = EmailAddress.create! valid_attributes
         put :update, {:id => email_address.to_param, :email_address => new_attributes}, valid_session
         email_address.reload
-        skip("Add assertions for updated state")
+        expect(email_address.address).to eq('updated_email@test.com')
+        expect(email_address.person_id).to eq(2)
       end
 
       it "assigns the requested email_address as @email_address" do
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
-  let(:email_address) { EmailAddress.new(address: 'test@example.com') }
+  let(:email_address) { EmailAddress.new(address: 'test@example.com', person_id: 1) }
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -9,6 +9,11 @@ RSpec.describe EmailAddress, :type => :model do
 
   it 'is invalid without an address' do
     email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'is invalid without a person_id' do
+    email_address.person_id = nil
     expect(email_address).to_not be_valid
   end
 end
